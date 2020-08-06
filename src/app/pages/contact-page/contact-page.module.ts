e73b258aca25ed6e8bf0f634df4668bb87b0a047
JorgeBdelaT <jabecerra@uc.cl>
2020-08-06 15:35:05 -0400

    refactor: retrieve contact  info from firestore

--- src/app/pages/contact-page/contact-page.module.ts
+++ src/app/pages/contact-page/contact-page.module.ts
@@ -4,8 +4,10 @@ import { CommonModule } from '@angular/common';
 import { ContactPageRoutingModule } from './contact-page-routing.module';
 import { ContactPageComponent } from './contact-page/contact-page.component';
 
+import { ComponentsModule } from 'src/app/components/components.module';
+
 @NgModule({
   declarations: [ContactPageComponent],
-  imports: [CommonModule, ContactPageRoutingModule],
+  imports: [CommonModule, ContactPageRoutingModule, ComponentsModule],
 })
 export class ContactPageModule {}

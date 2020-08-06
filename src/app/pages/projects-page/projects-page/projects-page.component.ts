89b34ee5b0aa73f7d360938697efe5f8fe884083
JorgeBdelaT <jabecerra@uc.cl>
2020-08-06 15:18:01 -0400

    refactor: projects content served from firebase

--- src/app/pages/projects-page/projects-page/projects-page.component.ts
+++ src/app/pages/projects-page/projects-page/projects-page.component.ts
@@ -3,6 +3,7 @@ import { ViewportScroller } from '@angular/common';
 import { Router } from '@angular/router';
 
 import { NavigationService } from 'src/app/services/navigation.service';
+import { FirestoreService } from 'src/app/services/firestore.service';
 
 @Component({
   selector: 'app-projects-page',
@@ -14,60 +15,30 @@ export class ProjectsPageComponent implements OnInit {
   justLoaded: boolean;
   scrollDirection: string;
 
-  data = [
-    {
-      title: 'smartform',
-      description:
-        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum repellat minus, exercitationem cupiditate commodi ipsum quos facere ratione! Animi perferendis aperiam nostrum et ipsa minima illo non temporibus ea fugiat.',
-      tasks: [
-        'Indolore laboriosam minima provident est, quos quidem',
-        'Praesentium qui ut indolore laboriosam minima provident est, quos quidem quibusdam eveniet distinctio tempora',
-        'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
-        'Grovident est, quos quidem qui ut indolore laboriosam',
-      ],
-      img: 'assets/images/smartform.png',
-      link: 'https://jorgebdelat.github.io/smart-form/',
-      repo: 'https://github.com/JorgeBdelaT/smart-form',
-    },
-    {
-      title: 'financepod',
-      description:
-        'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur voluptas eius, odit cupiditate amet, similique dicta vel dolores necessitatibus officiis deserunt. Delectus soluta aliquam deleniti odio et provident error commodi?',
-      tasks: [
-        'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
-        'Praesentium qui ut indolore laboriosam minima provident est, quos quidem quibusdam eveniet distinctio tempora',
-        'Indolore laboriosam minima provident est, quos quidem',
-        'Grovident est, quos quidem qui ut indolore laboriosam',
-      ],
-      img: 'assets/images/financepod.png',
-      link: 'http://financepod.herokuapp.com/',
-      repo: 'https://github.com/IIC2513-2019-2/grupo-casitas',
-    },
-    {
-      title: 'cverde page',
-      description:
-        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla a incidunt nam obcaecati optio corrupti, consectetur, porro harum officiis ex quis assumenda cumque similique, laboriosam voluptatibus enim saepe deleniti impedit.',
-      tasks: [
-        'Praesentium qui ut indolore laboriosam minima provident est, quos quidem quibusdam eveniet distinctio tempora',
-        'Indolore laboriosam minima provident est, quos quidem',
-        'Grovident est, quos quidem qui ut indolore laboriosam',
-        'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
-      ],
-      img: 'assets/images/cverde.png',
-      link: 'http://cverde.org/',
-      repo: 'https://github.com/savelozo/cverde-page',
-    },
-  ];
+  loading = true;
+  data = [];
 
   constructor(
     private router: Router,
     private viewportScroller: ViewportScroller,
-    private navigationService: NavigationService
+    private navigationService: NavigationService,
+    private firestoreService: FirestoreService
   ) {
     this.justLoaded = true;
   }
 
   ngOnInit(): void {
+    this.firestoreService.getResources('projects').subscribe((dataSnapshot) => {
+      this.data = [];
+      dataSnapshot.forEach((projectData: any) => {
+        this.data.push({
+          id: projectData.payload.doc.id,
+          ...projectData.payload.doc.data(),
+        });
+        this.loading = false;
+      });
+    });
+
     this.lastScroll =
       window.pageYOffset ||
       document.documentElement.scrollTop ||

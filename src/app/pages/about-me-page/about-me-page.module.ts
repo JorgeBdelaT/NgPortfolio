a17ef152400431f0d18dfc4f70f90a19f4782a28
JorgeBdelaT <jabecerra@uc.cl>
2020-08-06 18:12:24 -0400

    refactor: retrieve data for about page from firestore

--- src/app/pages/about-me-page/about-me-page.module.ts
+++ src/app/pages/about-me-page/about-me-page.module.ts
@@ -4,8 +4,10 @@ import { CommonModule } from '@angular/common';
 import { AboutMePageRoutingModule } from './about-me-page-routing.module';
 import { AboutMePageComponent } from './about-me-page/about-me-page.component';
 
+import { ComponentsModule } from 'src/app/components/components.module';
+
 @NgModule({
   declarations: [AboutMePageComponent],
-  imports: [CommonModule, AboutMePageRoutingModule],
+  imports: [CommonModule, AboutMePageRoutingModule, ComponentsModule],
 })
 export class AboutMePageModule {}
